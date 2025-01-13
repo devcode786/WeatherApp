57edc873756422b00e99c9fa7961a8861538cc98
Muhammad Farhan Shaukat <108552878+mbfarhanm1998@users.noreply.github.com>
2025-01-13 19:37:36 +0500

    'env'

--- src/services/weatherService.tsx
+++ src/services/weatherService.tsx
@@ -1,8 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
-import {API_KEY} from '@env';
+// import {API_KEY} from '@env';
 
-// const API_KEY=`45dc6281f938afab5f291854b289c8ae`
+const API_KEY=`45dc6281f938afab5f291854b289c8ae`
 // console.log(API_KEY)
 const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
