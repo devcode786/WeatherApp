c5503692909c11df79ccc3fe1da88ec85abf9930
Muhammad Farhan Shaukat <108552878+mbfarhanm1998@users.noreply.github.com>
2025-01-13 19:39:47 +0500

    'env'

--- src/services/weatherService.tsx
+++ src/services/weatherService.tsx
@@ -1,6 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
-// import {API_KEY} from '@env';
+import {API_KEY} from '@env';
 
 // const API_KEY=`45dc6281f938afab5f291854b289c8ae`
 // console.log(API_KEY)
